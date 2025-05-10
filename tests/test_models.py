f870316dbc3c173ba5e1c137c565bd6803e07c1b
MariaSchueller <mariaschueller@outlook.com>
2025-05-10 14:34:42 -0400

    tests updating product

--- tests/test_models.py
+++ tests/test_models.py
@@ -119,7 +119,24 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(product.available, found_product.available)
         self.assertEqual(product.category, found_product.category)
 
-        
+def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Change it an save it
+        product.description = "testing"
+        original_id = product.id
+        product.update()
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "testing")
+        # Fetch it back and make sure the id hasn't changed
+        # but the data did change
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].description, "testing")
 
 
 
